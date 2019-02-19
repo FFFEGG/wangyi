38b44b1b6709e77067c08940a1a772586f9cac49
FFFEGG <406811064@qq.com>
2019-02-19 17:42:46 +0800

    更换轮播图

--- lib/pages/index_gxtj.dart
+++ lib/pages/index_gxtj.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_pro/carousel_pro.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 class index_gxtj extends StatefulWidget {
   @override
@@ -33,27 +32,95 @@ class _index_gxtjState extends State<index_gxtj> {
             Positioned(
               top: 30.0,
               left: 10.0,
-              child:  Container(
-                  height: 120.0,
-                  width: 350.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white24,
+              child: Container(
+                height: 120.0,
+                width: MediaQuery.of(context).size.width - 20.0,
+                child: CarouselSlider(items: [
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/wa7Oxq5yysZgGBvOlivwEg==/109951163871318124.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                  child: Carousel(
-                    images: [
-                      new NetworkImage('http://p1.music.126.net/thHP6ooWw893Ccfk_iXG5w==/109951163866743145.jpg'),
-                      new NetworkImage('http://p1.music.126.net/kEGOOxh_p6H7UHKuOOFkFg==/109951163871789068.jpg'),
-                      new NetworkImage('http://p1.music.126.net/AKa1piGvA2WrH8NZLyAkrA==/109951163871321608.jpg'),
-                      new NetworkImage('http://p1.music.126.net/wa7Oxq5yysZgGBvOlivwEg==/109951163871318124.jpg'),
-                    ],
-                    dotSize: 4.0,
-                    dotSpacing: 14.0,
-                    dotColor: Colors.redAccent,
-                    indicatorBgPadding: 15.0,
-                    dotBgColor: Colors.white24.withOpacity(0.0),
-                    borderRadius: false,
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/kEGOOxh_p6H7UHKuOOFkFg==/109951163871789068.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/thHP6ooWw893Ccfk_iXG5w==/109951163866743145.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/AKa1piGvA2WrH8NZLyAkrA==/109951163871321608.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/JEnkpJ_K_FNFH74tpAusDQ==/109951163871768363.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    child: new Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://p1.music.126.net/x3GJj_0ke1XgIgkfdYLdqQ==/109951163871309165.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ], height: 120.0, autoPlay: true),
               ),
             ),
           ],
