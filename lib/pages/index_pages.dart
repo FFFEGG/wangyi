b37b71fcf43174660e76aab773d5dd494f711bf2
FFFEGG <406811064@qq.com>
2019-02-19 11:13:16 +0800

    首页顶部

---
+++ lib/pages/index_pages.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class indexpage extends StatefulWidget {
+  @override
+  _indexpageState createState() => _indexpageState();
+}
+
+class _indexpageState extends State<indexpage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1.0,
+          leading: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Image.asset(
+              'images/mic.png',
+              width: 5.0,
+              height: 5.0,
+            ),
+          ),
+          title: Container(
+            margin: EdgeInsets.all(10.0),
+            width: double.infinity,
+            height: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              border: Border.all(color: Colors.white30),
+              color: Colors.white30,
+            ),
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 20.0,
+                ),
+                Icon(
+                  Icons.search,
+                  size: 20.0,
+                  color: Colors.white54,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  '大家都在搜',
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    color: Colors.white54,
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  '龙卷风',
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    color: Colors.white54,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            Transform.rotate(
+              child: Icon(Icons.format_align_left),
+              angle: -1.6,
+            ),
+            SizedBox(
+              width: 16.0,
+            ),
+          ],
+          bottom: TabBar(
+//            //激活状态
+//            unselectedLabelColor: Colors.white30,
+//            //选中下先
+//            indicatorColor: Colors.white30,
+//            //大小
+            indicatorSize: TabBarIndicatorSize.label,
+////            //宽度
+////            indicatorWeight: 1.0,
+            tabs: [
+              Tab(
+                child: Text('个性推荐'),
+              ),
+              Tab(
+                child: Text('主播电台'),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Container(
+              child: Text('个性推荐'),
+            ),
+            Container(
+              child: Text('主播电台'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
